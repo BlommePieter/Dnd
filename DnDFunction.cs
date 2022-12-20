b31095ee907f948760d63fa0ecdda63ba852eaa3
Pieter <pieter.blomme@student.howest.be>
2022-12-20 12:15:16 +0100

    update

--- DnDFunction.cs
+++ DnDFunction.cs
@@ -357,6 +357,178 @@ public class DnDFunctions
         }
 
     }
+
+
+    [FunctionName("UpdateClass")]
+    public static async Task<IActionResult> UpdateClass(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "classes")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var class1 = JsonConvert.DeserializeObject<Class>(json);
+
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
+
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+
+            var container = client.GetContainer(General.COSMOS_DB_DND, General.COSMOS_CONTAINER_CLASSES);
+            await container.ReplaceItemAsync(class1, class1.Id);
+
+            return new OkObjectResult(class1);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
+
+    }
+
+    [FunctionName("UpdateSpell")]
+    public static async Task<IActionResult> UpdateSpell(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "spells")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var spell = JsonConvert.DeserializeObject<Spell>(json);
+
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
+
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+
+            var container = client.GetContainer(General.COSMOS_DB_DND, General.COSMOS_CONTAINER_SPELLS);
+            await container.ReplaceItemAsync(spell, spell.Id);
+
+            return new OkObjectResult(spell);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
+
+    }
+
+    [FunctionName("UpdateItem")]
+    public static async Task<IActionResult> UpdateItem(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "items")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var item = JsonConvert.DeserializeObject<Item>(json);
+
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
+
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+
+            var container = client.GetContainer(General.COSMOS_DB_DND, General.COSMOS_CONTAINER_ITEMS);
+            await container.ReplaceItemAsync(item, item.Id);
+
+            return new OkObjectResult(item);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
+
+    }
+
+    [FunctionName("UpdateWeapon")]
+    public static async Task<IActionResult> UpdateWeapon(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "weapons")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var weapon = JsonConvert.DeserializeObject<Weapon>(json);
+
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
+
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+
+            var container = client.GetContainer(General.COSMOS_DB_DND, General.COSMOS_CONTAINER_WEAPONS);
+            await container.ReplaceItemAsync(weapon, weapon.Id);
+
+            return new OkObjectResult(weapon);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
+
+    }
+
+    [FunctionName("UpdateArmor")]
+    public static async Task<IActionResult> UpdateArmor(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "armors")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var armor = JsonConvert.DeserializeObject<Armor>(json);
+
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
+
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+
+            var container = client.GetContainer(General.COSMOS_DB_DND, General.COSMOS_CONTAINER_ARMOR);
+            await container.ReplaceItemAsync(armor, armor.Id);
+
+            return new OkObjectResult(armor);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
+
+    }
 }
+
 
 
